f87ab1979fcf99f435b0c36ac596242975441e0a
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-01-26 21:47:44 +0800

    Update default IP and hostname in diy-part2.sh
    Modify default IP and set hostname in config_generate

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-# 暂时留空
+# diy-part2.sh
+
+# --- 1. 修改默认 IP 为 192.168.66.1 ---
+# 这行命令会强制把系统里的默认 IP 替换掉
+sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
+
+# --- 2. (可选) 设置主机名 ---
+sed -i 's/ImmortalWrt/N3061-Router/g' package/base-files/files/bin/config_generate
