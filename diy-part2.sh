24f58df5f18bcaf03c8083899ae99f3b8d546d65
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-01-27 19:57:42 +0800

    Update diy-part2.sh for theme and IP changes

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# diy-part2.sh
 
 # --- 1. 强制修改默认 IP 为 192.168.66.1 ---
 sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 
-# --- 2. 下载第三方插件源码 (核心步骤) ---
-# [QModem] 下载 FUjr 版本，提供 RM500Q 的图形管理界面
-git clone https://github.com/FUjr/luci-app-qmodem.git package/new/luci-app-qmodem
-
-# [Argon] 下载 iStoreOS 同款紫色主题和设置插件
+# --- 2. 下载 Argon 皮肤 (iStoreOS 风格) ---
+# 仅美化界面，不包含商店
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/new/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/new/luci-app-argon-config
 
