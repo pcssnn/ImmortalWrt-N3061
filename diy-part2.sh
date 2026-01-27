2979826b770254973d3f1bc52004ddd36c009e44
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-01-27 22:39:13 +0800

    Update theme installation and remove duplicate command
    Removed duplicate QModem download command and updated theme installation.

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,8 +4,10 @@
 # --- 1. 强制修改默认 IP 为 192.168.66.1 ---
 sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 
-# --- 2. 下载 Argon 皮肤 (iStoreOS 风格) ---
-# 仅美化界面，不包含商店
+# --- [重要] 移除了 QModem 下载命令 ---
+# 原因：已在 diy-part1.sh 中下载，此处重复会导致报错！
+
+# --- 2. 下载界面美化插件 (Argon) ---
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/new/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/new/luci-app-argon-config
 
