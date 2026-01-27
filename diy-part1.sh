cfb8b1228344f782caf17f75a795773e79a08d85
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-01-27 19:56:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,11 +2,11 @@
 # diy-part1.sh
 
 # --- 重点：下载您指定的 FUjr 版 QModem ---
-# 这行代码会去下载 FUjr 的项目代码
+# 用于 5G 模组图形化管理
 git clone https://github.com/FUjr/luci-app-qmodem package/luci-app-qmodem
 
 # --- 补充：短信工具 ---
-# QModem 自带短信管理，但为了防止不兼容，添加一个通用的 SMS 工具作为备份
+# 作为 QModem 的备份工具
 git clone https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
 
 # 添加其他可能需要的依赖
