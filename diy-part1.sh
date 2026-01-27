ed0cf32978339eb563abd65f2194989b23f67283
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-01-28 01:08:47 +0800

    Update SMS tool repository clone command

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,6 +7,3 @@ git clone https://github.com/FUjr/QModem.git package/QModem
 
 # --- 补充：短信工具 ---
 git clone https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
-
-# 添加其他可能需要的依赖
-# echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
