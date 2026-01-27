f9bbab7f38691518d390a1e5a00a802feab270c7
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-01-28 01:09:43 +0800

    Add iStore and related UI for application management
    Added iStore application store for Docker apps and plugins.

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,8 +4,10 @@
 # --- 1. 强制修改默认 IP 为 192.168.66.1 ---
 sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 
-# --- [重要] 移除了 QModem 下载命令 ---
-# 原因：已在 diy-part1.sh 中下载，此处重复会导致报错！
+# --- [新增] iStore 应用商店 ---
+# 有了它，你可以像手机一样安装 Docker 应用和插件
+git clone https://github.com/linkease/istore.git package/new/luci-app-store
+git clone https://github.com/linkease/istore-ui.git package/new/luci-app-store-ui
 
 # --- 2. 下载界面美化插件 (Argon) ---
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/new/luci-theme-argon
