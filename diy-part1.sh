f0e2936fc339cf5aca3f022fb6bf71f393f7ee8f
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-02 17:04:40 +0800

    Update software sources in diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # diy-part1.sh
 
-# --- [修复] 引入高质量第三方源 (Kenzo & Small) ---
-# 这是解决 AdGuard Home 和 Tailscale 缺失的关键
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+# --- [核心] 引入 Kiddin9 软件源 ---
+# 这是一个非常知名的聚合源，解决了 iStore 和 Tailscale 的依赖问题
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
 
-# --- QModem 驱动 (5G 模组) ---
+# --- QModem 5G 驱动 (单独保留，官方源更好用) ---
 git clone https://github.com/FUjr/QModem.git package/QModem
-
-# --- 短信工具 ---
 git clone https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
