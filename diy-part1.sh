a47626a5df5c3c2fc8c2018d84611aedd07fef03
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-02 23:36:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # diy-part1.sh
 
-# --- 1. 清理潜在的干扰源 ---
-# 如果默认源里有重复的，先注释掉，防止冲突
-# (通常不需要，但为了保险起见)
-
-# --- 2. 引入 Kenzo/Small 源 (稳健、快速) ---
-# 只有这两个源，没有 Kiddin9
+# --- 1. 软件源配置 ---
+# 使用 Kenzo 和 Small 源，它们包含了 AdGuardHome 和 Tailscale，且下载稳定
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# --- 3. 5G 驱动与工具 ---
+# --- 2. 5G 模组驱动与工具 ---
+# 移远 RM500Q 专用管理面板
 git clone https://github.com/FUjr/QModem.git package/QModem
+# 短信收发工具
 git clone https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
