5c6d74bcab9a9fc531bbef0a8b908f42068aa3bb
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-02 18:22:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # diy-part1.sh
 
-# --- [核心] 引入 Kiddin9 软件源 ---
-# 这是一个非常知名的聚合源，解决了 iStore 和 Tailscale 的依赖问题
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >> feeds.conf.default
+# --- [修复] 移除不稳定的巨型源，使用 Kenzo/Small (速度快，包含 Tailscale/AGH) ---
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# --- QModem 5G 驱动 (单独保留，官方源更好用) ---
+# --- QModem 5G 驱动 (保持不变) ---
 git clone https://github.com/FUjr/QModem.git package/QModem
 git clone https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
