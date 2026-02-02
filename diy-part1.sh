b47c72cba94f7cb1f33289c6465a3bb48e4be7d8
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-02 22:00:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # diy-part1.sh
 
-# --- [修复] 移除 Kiddin9 (太大导致下载失败)，改用 Kenzo/Small ---
-# Kenzo 源包含 AdGuardHome 和 Tailscale，下载速度快，不报错
+# --- 1. 清理潜在的干扰源 ---
+# 如果默认源里有重复的，先注释掉，防止冲突
+# (通常不需要，但为了保险起见)
+
+# --- 2. 引入 Kenzo/Small 源 (稳健、快速) ---
+# 只有这两个源，没有 Kiddin9
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# --- QModem 5G 驱动 (保持不变) ---
+# --- 3. 5G 驱动与工具 ---
 git clone https://github.com/FUjr/QModem.git package/QModem
 git clone https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
