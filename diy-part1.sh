0cc26ce14883be3f3ce660ed7935d34948bf695f
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-03 11:39:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,12 +2,10 @@
 # diy-part1.sh
 
 # --- 1. 软件源配置 ---
-# 使用 Kenzo 和 Small 源，它们包含了 AdGuardHome 和 Tailscale，且下载稳定
+# Kenzo/Small 包含 Tailscale 和 AdGuardHome，且下载稳定
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# --- 2. 5G 模组驱动与工具 ---
-# 移远 RM500Q 专用管理面板
+# --- 2. 5G 模组驱动 ---
 git clone https://github.com/FUjr/QModem.git package/QModem
-# 短信收发工具
 git clone https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
