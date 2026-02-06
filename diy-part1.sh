ccd4d9794d35ef6e86e34daf746c7bb450b975f9
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-06 11:32:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # diy-part1.sh
 
-# --- 1. 软件源 ---
-# 引入 Kenzo 源
+# --- 1. 引入高质量软件源 ---
+# 包含 Tailscale, AdGuardHome 等常用插件
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
