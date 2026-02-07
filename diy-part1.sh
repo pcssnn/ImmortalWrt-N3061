b1d49d0f1e58afbde4db2f708c2a94eef702bd13
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-07 18:45:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # diy-part1.sh
 
-# --- 1. 引入高质量软件源 ---
-# 包含 Tailscale, AdGuardHome 等常用插件
+# --- 1. 核心插件源 (Kenzo) ---
+# 包含 KMS, Tailscale, AdGuardHome 等
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
 # --- 2. 5G 模组驱动 ---
 git clone https://github.com/FUjr/QModem.git package/QModem
 git clone https://github.com/4IceG/luci-app-sms-tool package/luci-app-sms-tool
+
+# --- 3. Argon 主题 ---
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
