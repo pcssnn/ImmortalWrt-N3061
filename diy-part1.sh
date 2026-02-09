bf9aeff235dd43cfe73c1bae24b2da66530def94
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-09 09:48:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # diy-part1.sh
 
-# 1. 基础依赖源
+# 1. 还原：Kenzo 核心源 (当时 AdGuard 等就是靠它)
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# 2. iStore 商店 (Master源)
+# 2. 还原：iStore 官方源
 echo 'src-git istore https://github.com/linkease/istore;master' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git ui https://github.com/linkease/istore-ui.git;master' >> feeds.conf.default
 
-# 3. 核心功能源码植入 (package/custom)
+# 3. 还原：源码植入 (这是当时成功的关键)
 mkdir -p package/custom
 
-# 5G拨号 & 短信
+# 5G & 短信
 git clone https://github.com/FUjr/QModem.git package/custom/QModem
 git clone https://github.com/4IceG/luci-app-sms-tool.git package/custom/luci-app-sms-tool
 
-# 去广告 (源码版)
+# AdGuardHome (源码版最稳)
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/custom/luci-app-adguardhome
 
-# Tailscale (源码版)
+# Tailscale
 git clone https://github.com/asvow/luci-app-tailscale.git package/custom/luci-app-tailscale
 
-# Argon 主题
+# 主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
