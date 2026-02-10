b787f038cfda48c0272c54f1dd746f58cc908ec5
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-10 11:17:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 # diy-part1.sh
 
-# 1. 基础源 (Kenzo 在 v23.05 下非常稳)
+# 1. 基础源 (Kenzo 包含 HomeProxy 依赖)
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# 2. iStore (仅引入商店核心，不预装 NAS APP)
-echo 'src-git istore https://github.com/linkease/istore' >> feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git' >> feeds.conf.default
-echo 'src-git ui https://github.com/linkease/istore-ui.git' >> feeds.conf.default
+# 2. iStore (⚠️强力措施：强制使用 master 分支源，适配 APK)
+echo 'src-git istore https://github.com/linkease/istore;master' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git ui https://github.com/linkease/istore-ui.git;master' >> feeds.conf.default
 
-# 3. 核心功能源码植入 (package/custom)
+# 3. 核心源码植入 (⚠️强力措施：git clone 暴力覆盖)
 mkdir -p package/custom
 
-# 5G 拨号 (指定 FUjr 源码)
+# 5G 拨号 (FUjr 源码 - 您指定的)
 git clone https://github.com/FUjr/QModem.git package/custom/QModem
 
-# 短信工具 (指定 4IceG 源码)
+# 短信工具 (4IceG 源码 - 您指定的)
 git clone https://github.com/4IceG/luci-app-sms-tool.git package/custom/luci-app-sms-tool
+
+# Tailscale (内网穿透 - asvow版最稳)
+git clone https://github.com/asvow/luci-app-tailscale.git package/custom/luci-app-tailscale
+
+# HomeProxy (家庭代理 - 确保源码存在)
+git clone https://github.com/immortalwrt/homeproxy.git package/custom/homeproxy
 
 # AdGuardHome (源码版)
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/custom/luci-app-adguardhome
 
-# Tailscale (内网穿透)
-git clone https://github.com/asvow/luci-app-tailscale.git package/custom/luci-app-tailscale
-
-# Argon 主题 (iStore 风格)
+# Argon 主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
