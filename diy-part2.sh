b7563d6d9ca7b1ad7d4e6d9f06fbce0a51536c28
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-10 11:18:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,23 +4,23 @@
 # 1. 设置 IP: 192.168.66.1
 sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修复 Go 环境 (v23.05 必须升级 Go 才能编译 ADG)
+# 2. 修复 Go 环境 (⚠️强力措施：Master 编译 ADG 必备，不修必挂)
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 rm -rf package/feeds/packages/golang
 ./scripts/feeds install -p packages -f golang
 
-# 3. 替换为国内源 (北大源)
+# 3. 替换为国内源
 sed -i 's/downloads.immortalwrt.org/mirrors.pku.edu.cn\/immortalwrt/g' package/base-files/files/bin/config_generate
 
-# 4. 强制中文 & 主题设置
+# 4. 强制中文 & 主题 & 主机名
 mkdir -p package/base-files/files/etc/uci-defaults
 cat > package/base-files/files/etc/uci-defaults/99-custom-settings <<EOF
 #!/bin/sh
 uci set luci.main.lang=zh_cn
 uci set luci.main.mediaurlbase=/luci-static/argon
 uci set luci.themes.Argon=/luci-static/argon
-uci set system.@system[0].hostname='N3061-Gateway'
+uci set system.@system[0].hostname='N3061-Master'
 uci commit luci
 uci commit system
 exit 0
