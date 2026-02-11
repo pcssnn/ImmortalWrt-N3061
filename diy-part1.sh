9d88082bd1f6cd0a46b390922a6abdded3b2060b
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-11 13:03:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 # diy-part1.sh
 
-# 1. 基础源
+# 1. ??【复刻关键】使用 Kenzo 聚合源
+# 这是 2月3日 版本功能齐全的核心原因，它包含大量兼容性补丁
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-# 2. iStore (Master 分支适配)
+# 2. iStore (官方源)
 echo 'src-git istore https://github.com/linkease/istore;master' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git ui https://github.com/linkease/istore-ui.git;master' >> feeds.conf.default
 
-# 3. 核心源码植入 (package/custom 下优先级最高)
+# 3. 手动修正与补充
 mkdir -p package/custom
 
-# 🔥【Docker 修复】(解决红框报错的关键)
-# 先删除 feed 里可能有问题的版本
+# ??【Docker 修正】
+# Kenzo 源里的 Docker 可能与最新 Master 有冲突，我们用 lisaac 原版覆盖它
 rm -rf feeds/luci/applications/luci-app-dockerman
-# 拉取 lisaac 原作者的修复代码
 git clone https://github.com/lisaac/luci-app-dockerman.git package/custom/luci-app-dockerman
 git clone https://github.com/lisaac/luci-lib-docker.git package/custom/luci-lib-docker
 
-# 🔥【5G & SMS】
+# ??【皮肤锁定】Argon
+# 您强调皮肤不能变，我们手动拉取最新版
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
+
+# ??【5G & SMS】
 git clone https://github.com/FUjr/QModem.git package/custom/QModem
 git clone https://github.com/4IceG/luci-app-sms-tool.git package/custom/luci-app-sms-tool
 
-# 【网络工具】
-git clone https://github.com/immortalwrt/homeproxy.git package/custom/homeproxy
-git clone https://github.com/asvow/luci-app-tailscale.git package/custom/luci-app-tailscale
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/custom/luci-app-adguardhome
-
-# 【主题】
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
+# 注意：HomeProxy, AdGuard, KMS, Tailscale 等现在直接从 Kenzo 源获取
+# 这样兼容性最好，复刻了之前的成功环境。
