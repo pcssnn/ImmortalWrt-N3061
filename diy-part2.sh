d20e8a195f017b0e4010d6ef0f959280fa6d3147
悠悠然 <127330586+pcssnn@users.noreply.github.com>
2026-02-11 16:33:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 # diy-part2.sh
 
-# 1. 设置 IP: 192.168.66.1
+# 1. 设置 IP
 sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修复 Go 环境 (ADG 编译必备)
+# 2. 🔥【暴力汉化】源码级锁定
+# 修改默认语言 auto -> zh_cn
+sed -i "s/option lang 'auto'/option lang 'zh_cn'/g" package/base-files/files/bin/config_generate
+
+# 3. 修复 Go 环境
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 rm -rf package/feeds/packages/golang
 ./scripts/feeds install -p packages -f golang
 
-# 3. 🔥【核心】强制锁定中文
-# 直接修改 config_generate 脚本，让出厂默认就是 zh_cn
-sed -i "s/option lang 'auto'/option lang 'zh_cn'/g" package/base-files/files/bin/config_generate
+# 4. 🔥【依赖欺骗】强行修复 Dockerman
+# 确保它依赖我们拉取的库，而不是系统不存在的库
+sed -i 's/DEPENDS:=.*/DEPENDS:=+luci-lib-docker +luci-lib-jsonc/g' package/custom/luci-app-dockerman/Makefile
 
-# 4. 🔥【核心】强制锁定 Argon 皮肤
-# 确保 Argon 成为默认主题
+# 5. 🔥【主题锁定】Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 5. 国内源 & 辅助设置
+# 6. 国内源 & 辅助
 sed -i 's/downloads.immortalwrt.org/mirrors.pku.edu.cn\/immortalwrt/g' package/base-files/files/bin/config_generate
 mkdir -p package/base-files/files/etc/uci-defaults
 cat > package/base-files/files/etc/uci-defaults/99-custom-settings <<EOF
@@ -26,7 +29,7 @@ cat > package/base-files/files/etc/uci-defaults/99-custom-settings <<EOF
 uci set luci.main.lang=zh_cn
 uci set luci.main.mediaurlbase=/luci-static/argon
 uci set luci.themes.Argon=/luci-static/argon
-uci set system.@system[0].hostname='N3061'
+uci set system.@system[0].hostname='N3061-Master'
 uci commit luci
 uci commit system
 exit 0
